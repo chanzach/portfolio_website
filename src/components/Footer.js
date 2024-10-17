d02b49307ffe7f2568077ad3025bf4c1b650e745
zachdchan3@gmail.com <zacharychan@Zacharys-MacBook-Air-5.local>
2024-10-17 17:14:46 -0400

    Pages, basic setup

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Footer = () => (
+    <footer>
+        <p>&copy; 2024 My Name. All rights reserved.</p>
+    </footer>
+);
+
+export default Footer;
---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Home = () => (
+    <main>
+        <section className="hero">
+            <h1>Welcome to My Portfolio</h1>
+            <p>UI/UX Designer & Developer</p>
+        </section>
+        <section className="featured-projects">
+            <h2>Featured Projects</h2>
+            {/* Add ProjectCards here */}
+        </section>
+    </main>
+);
+
+export default Home;
